dfb331f8dac71c5d0dffba9daacca7ae6293c3c1
AAKASH-2207 <akshisharma2207@gmail.com>
2026-05-20 11:09:38 +0530

    added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Hero from "../components/Hero";
 import Navbar from "../components/navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+
 export default function Home() {
   return (
     <main className="bg-[#060B14] text-white min-h-screen">
